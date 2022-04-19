db74dee650305e826566b80c6ff4fe28a18fbc09
Ha <nguyenhoangha1342001@gmail.com>
2022-04-19 15:37:54 +0700

    "Update Login"

---
+++ btlAPI/Areas/Client/TB_YTEController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace btlAPI.Areas.Client
+{
+    public class TB_YTEController : Controller
+    {
+        // GET: Client/TB_YTE
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ btlAPI/Controllers/LoginController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using btlAPI.Models;
+
+namespace btlAPI.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        BTL_WebEntities1 db = new BTL_WebEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Login(string TenDangNhap, string MatKhau)
+        {
+            TaiKhoan tk = db.TaiKhoans.Where(s => s.UserName == TenDangNhap).SingleOrDefault<TaiKhoan>();
+            if (tk == null)
+            {
+                TempData["Err"] = "Tài khoản không hợp lệ!";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                if (tk.Password == MatKhau)
+                {
+
+                    ChuQuanLy user = db.ChuQuanLies.Where(s => s.ID == tk.ID).SingleOrDefault<ChuQuanLy>();
+                    Session["User"] = user.Ten;
+                    Session["TK"] = tk.Password;
+                    if (Session["Type"].ToString().ToUpper().Equals("Admin".ToUpper()))
+                    {
+                        return RedirectToAction("Index", "Admin/NhanVien");
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Client/TB_YTE");
+                    }
+                }
+            }
+            TempData["Err"] = "Sai mật khẩu!";
+            return RedirectToAction("Index");
+
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
